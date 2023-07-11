5b697503a215863118d30ceb0a8a4977b531c7ca
0x1e-robi <robelmezemir@gmail.com>
2023-07-11 12:29:20 +0300

    path shit ?

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,15 +4,20 @@
 // import * as d3 from 'd3';
 // import { json } from '@sveltejs/kit';/
 import { readFile } from 'fs/promises';
+import { join } from 'path';
 import type { PageServerLoad } from './(app)/matter/$types';
 
 export const load: PageServerLoad = async () => {
-	const fileContent = await readFile("./static/datasets/outf.json", 'utf-8');
-	const cfileContent = await readFile("./static/datasets/colors.json", 'utf-8');
+	const filePath1 = join(process.cwd(), 'static', 'datasets', 'outf.json');
+	const filePath2 = join(process.cwd(), 'static', 'datasets', 'colors.json');
+
+	const fileContent = await readFile(filePath1, 'utf-8');
+	const cfileContent = await readFile(filePath2, 'utf-8');
+
 	const jsonData = JSON.parse(fileContent)
 	const colors = JSON.parse(cfileContent)
 	// Preprocess jsonData to remove objects without a 'from' attribute
 	const preprocessedData = jsonData.filter((obj) => 'from' in obj);
 
-	return { jsonData: preprocessedData , colors : colors};
+	return { jsonData: preprocessedData, colors: colors };
 };
