58c48a5f620c2e546fb67826be3b3c3567b1d1b4
0x1e-robi <robelmezemir@gmail.com>
2023-07-11 12:32:00 +0300

    ree

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,16 +3,15 @@
 
 // import * as d3 from 'd3';
 // import { json } from '@sveltejs/kit';/
-import { readFile } from 'fs/promises';
+import { readFile } from 'fs';
 import { join } from 'path';
-import type { PageServerLoad } from './(app)/matter/$types';
 
 export const load: PageServerLoad = async () => {
 	const filePath1 = join(process.cwd(), 'static', 'datasets', 'outf.json');
 	const filePath2 = join(process.cwd(), 'static', 'datasets', 'colors.json');
 
-	const fileContent = await readFile(filePath1, 'utf-8');
-	const cfileContent = await readFile(filePath2, 'utf-8');
+	const fileContent = readFile(filePath1, 'utf-8');
+	const cfileContent = readFile(filePath2, 'utf-8');
 
 	const jsonData = JSON.parse(fileContent)
 	const colors = JSON.parse(cfileContent)
